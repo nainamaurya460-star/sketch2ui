9c45cad221ed45271db37f6a3f743e5515bb038f
shikha Yadav <shikhayadavhasnapur@gmail.com>
2026-07-31 15:58:58 +0530

    Day 4 complete: Optimized contour extraction with hierarchy and area filtering

--- backend/app/services/contour_services.py
+++ backend/app/services/contour_services.py
@@ -50,3 +50,41 @@ def clean_and_blur_image(image_path:str):
     blurred_img = cv2.GaussianBlur(img, (5, 5), 0)
     binary = cv2.adaptiveThreshold(blurred_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     return binary
+def extract_contours(image: np.ndarray):
+    """Image me se shapes detect karke filtered bounding boxes nikalta hai"""
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    bounding_boxes = []
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 100:
+            x, y, w, h = cv2.boundingRect(cnt)
+            bounding_boxes.append({"x": int(x), "y": int(y), "width": int(w), "height": int(h)})
+            
+    return bounding_boxes
+def extract_contours(image: np.ndarray):
+    """Image me se shapes detect karke filtered bounding boxes nikalta hai"""
+    if len(image.shape) == 3:  # Agar image color hai
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = image
+    #Noise reduction & thresholding( Day 3 logic)
+    
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    # hierarchy-based contour extraction(RETR_TREE)
+    contours, hierarchy = cv2.findContours( thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    bounding_boxes = []
+    img_h, img_w = image.shape[:2]
+    max_allowed_area = (img_h * img_w) * 0.9  # 90% of the image area ignorance
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        x, y, w, h = cv2.boundingRect(cnt)
+        """ Filtering . 1: Minimum size constraint: width & height > 15px, area > 100px. 2: Maximum size constraint: area < 90% of the image area. 3: Aspect ratio constraint: width/height between 0.2 and 5.0 """
+        if 100 < area < max_allowed_area and w > 15 and h > 15:
+         bounding_boxes.append({"x": int(x), "y": int(y), "width": int(w), "height": int(h)})
+           
+    return bounding_boxes
